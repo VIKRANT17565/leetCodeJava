ea68abf8b81ef454282a6d8146516458c2331290
Vikrant Singh <vikrantdlsingh518@gmail.com>
2022-07-25 22:06:43 +0530

    first commit

---
+++ TwoSum_1.java
@@ -0,0 +1,29 @@
+public class TwoSum_1 {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        twoSum(nums, 9);
+        
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        boolean flag = false;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (nums[i]+nums[j] == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag) {
+                break;
+            }
+        }
+        return ans;
+    }
+}
